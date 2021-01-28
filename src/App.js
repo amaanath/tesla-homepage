07d8d8a983404dffccfdac947179a63d7064f6ec
Amaanath Mumtaz <amaanathmumtaz@gmail.com>
2021-01-29 01:59:58 +0530

    Header + Assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-
+import Header from './components/Header'
 function App() {
   return (
     <div className="App">
-      <h1>Hello World 🚀</h1>
+      <Header />
+      {/* Products => Scroll Snapping */}
+      {/* Products => Scroll Snapping */}
+      {/* Products => Scroll Snapping */}
+      {/* Products => Scroll Snapping */}
+      {/* Products => Scroll Snapping */}
+      {/* Products => Scroll Snapping */}
+      {/* Products => Scroll Snapping */}
+      {/* Footer */}
     </div>
   );
 }
